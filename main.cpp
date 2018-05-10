ac633eb97cdf24cb69c141fa46bdb5888fe22627
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 11:20:36 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int furlongs;
+    cin>>furlongs;
+    int yard;
+    yard= 220*furlongs;
+    cout<<furlongs<<" furlongs = "<< yard<< "  yards";
+    return 0;
+}
