c9a7544abd24fbf34af21ddc77f5ae00a63ca0a9
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 14:44:46 +0400

    Update 5.7.cpp

--- 5.7.cpp
+++ 5.7.cpp
@@ -2,7 +2,7 @@
 #include <string>
 struct Cars
 {
-std::string make;
+std::string maker;
 int year;
 };
 int main()
@@ -10,6 +10,20 @@ int main()
   std::cout<<"How many cars are in catalog: ";
   int count;
   std::cin>>count;
-  Cars *pc=new Cars[];
-
+  Cars *pc=new Cars[count];
+ for(int i = 1; i <=count; i++)
+    {
+  std::cout << "Car #" << i << ":" << std::endl;
+  std::cout << "Please enter the maker: ";
+  getline(std::cin, pc[i].maker);
+  std::cout << "Please enter the year made: ";
+  std::cin >>pc[i].year;
+  std::cin.get();
+    }
+  std::cout<<"Here is your collection: "<<std::endl;
+  for(int i = 1; i <=count; i++)
+   std::cout<<pc[i].year << " " << pc[i].maker << std::endl;
+   
+   delete [] pc;
+   return 0;
 }
