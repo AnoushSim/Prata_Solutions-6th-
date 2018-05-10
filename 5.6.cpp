4ddec94abf3fd2518c309b33808281e7b447ef2f
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 14:28:24 +0400

    Update 5.6.cpp

--- 5.6.cpp
+++ 5.6.cpp
@@ -2,24 +2,22 @@
 #include <string>
 const int n=12;
 int main()
-{  int i,j,s=0;
+{  int j,s=0;
    std::string months[n]= { "January", "February", "March", 
    "April", "May", "June",
         "July", "August", "September", 
         "October", "November",  "December" }; 
-   int  books_of_eachmonth[5][n];
-   for( i = 0; i <5; i++)
-    {
-    std::cout << "Year " << i << ": " << std::endl;
+   int  books_of_eachmonth[1][n];
+  
     for( j = 0; j < n; j++)
       {
             std::cout << "Enter sale for " << months[j] << ": ";
-            std::cin >> books_of_eachmonthy[i][j];
+            std::cin >> books_of_eachmonthy[1][j];
         }
-    }
-   for(i=0;i<n;i++)
-   { for( j = 0; j < n; j++)
-    s+=books_of_eachmonth[i][n];
-    std::cout<<"You sold "<< s<<" books.";}
+     for( j = 0; j < n; j++)
+     {
+        s+=books_of_eachmonth[1][j];
+    std::cout<<"You sold "<< s<<" books.";
+     }
     return 0;
 }
