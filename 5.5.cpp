e1f504ce5047d373a45afeabb0447ce8046be647
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 13:40:35 +0400

    Create 5.5.cpp

---
+++ 5.5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+const int n=12;
+int main()
+{   
+    int i,s;
+    int sale[n];
+    std::string months[12] = {"January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November",
+        "December" };
+    
+    for(i=0;i<n;i++)
+    {
+       std::cout<<"Enter the number of books in "<<months[i]<<": ";
+       std::cin>>sale[i];
+    }
+   
+    for(i=0;i<n;i++)
+    s+=sale[i];
+    std::cout<<"You sold "<< s<<" books.";
+    return 0;
+}
