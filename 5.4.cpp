f022007d6488b69958e8b5426bce5f84fd7f6942
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 13:11:23 +0400

    Create 5.4.cpp

---
+++ 5.4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{   using namespace std;
+    double daphne = 100.0;
+    double cleo = 90.0;
+    int year;
+    
+    for(year = 1; cleo < daphne; ++year)
+    {
+        daphne += 10.0;
+        cleo *= 1.05;
+    }
+    
+    cout << year << " years later, " << endl;
+    cout << "Daphne has $" << daphne << endl;
+    cout << "Cleo has $" << cleo << endl;
+    
+    return 0;
+}
