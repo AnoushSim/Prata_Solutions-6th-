be76eb8c94610c4b98aac91f2f157dde0b9026fe
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 11:55:12 +0400

    Create 4.3.cpp

---
+++ 4.3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{  
+    const int n=20;
+    char name[n];
+    char lname[n];
+    char fname[2*n];
+    std::cout<<"Enter Your Firts name: ";
+    std::cin.getline(name,n);
+    std::cout<<"Enter Your Firts name: ";
+    std::cin.getline(lname,n);
+    strcpy(fname, lname);
+    strcat(fname, ", ");
+    strcat(fname, name);
+    
+    std::cout << "Here's the information in a single string: ";
+    std::cout << fname << endl;
+    return 0;
+}
