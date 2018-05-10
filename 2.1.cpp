396c5283f333402172e9759675714f76d7ffbaa0
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 11:21:59 +0400

    Update and rename main.cpp to 2.1.cpp

---
+++ 2.1.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int furlongs;
+    cin>>furlongs;
+    int yard;
+    yard= 220*furlongs;
+    cout<<furlongs<<" furlongs = "<< yard<< "  yards";
+    return 0;
+}
