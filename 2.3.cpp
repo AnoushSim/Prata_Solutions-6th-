335f2d2ac87e0ae115b0cde6d38d7b85acea55f1
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-10 11:26:22 +0400

    Create 2.3.cpp

---
+++ 2.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void f1(void);
+void f2(void);
+int main()
+{
+    f1();
+    f1();
+    f2();
+    f2();
+    return 0;
+}
+void f1(void)
+{
+    cout<<"Three blind mice"<<endl;
+}
+void f2(void)
+{
+    cout<<"See how they run"<<endl;
+}
